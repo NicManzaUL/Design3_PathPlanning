8c75fa880c25b6951955f5b96513d30bb1aedb9d
Nicholas Manzano <NIMAN16@ulaval.ca>
2023-02-21 12:39:09 -0500

    scale

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append("../")
 from pathPlanning.Navigation import runScenario1
-
+from pathPlanning.Navigation import runScenario2
 if __name__ == '__main__':
     print(__file__ + " start!!")
-    runScenario1()
+    runScenario2()
     print(__file__ + " Done!!")
